7ece9de4cf288b764671cee58f3a5e13cdbf2322
Mark Meliksetyan <markmelix@gmail.com>
2022-06-12 16:43:08 +0300

    Make high-level configuration of penrose

--- src/lib.rs
+++ src/lib.rs
@@ -3,24 +3,88 @@ extern crate penrose;
 
 use result::Result;
 
+/// Run Window Manager.
 pub fn run() -> Result<()> {
-	use penrose::{
-		logging_error_handler, xcb::new_xcb_backed_window_manager, Config,
-	};
+	use penrose::{logging_error_handler, xcb::new_xcb_backed_window_manager};
 
 	let (key_bindings, mouse_bindings) = bindings::both();
 
-	let mut wm = new_xcb_backed_window_manager(
-		Config::default(),
-		vec![],
-		logging_error_handler(),
-	)?;
+	let config = config::get()?;
+	let hooks = vec![];
+	let error_handler = logging_error_handler();
+
+	let mut wm = new_xcb_backed_window_manager(config, hooks, error_handler)?;
 
 	Ok(wm.grab_keys_and_run(key_bindings, mouse_bindings)?)
 }
 
+pub mod config {
+	use crate::result::Result;
+	use penrose::Config;
+
+	/// Default terminal emulator
+	pub const TERMINAL: &str = "alacritty";
+
+	/// Names of the workspaces
+	pub const WORKSPACES: [&str; 9] =
+		["term", "code", "web", "other", "5", "6", "7", "8", "9"];
+
+	/// Classes of the floating windows
+	pub const FLOATING_CLASSES: Vec<&str> = vec![];
+
+	/// Color of focused window border
+	pub const FOCUSED_BORDER_COLOR: &str = "#FFFFFF";
+
+	/// Color of unfocused window border
+	pub const UNFOCUSED_BORDER_COLOR: &str = "#000000";
+
+	/// Border width in pixels
+	pub const BORDER_WIDTH: u32 = 2;
+
+	/// Size of gap between windows in pixels
+	pub const GAP_SIZE: u32 = 5;
+
+	/// The percentage of the screen to grow the main region by when incrementing
+	pub const MAIN_RATIO_STEP: f32 = 0.;
+
+	/// Whether or not space should be reserved for a status bar
+	pub const SHOW_BAR: bool = true;
+
+	/// Whether or not the reserved space for a status bar is at the top of the sceen
+	pub const TOP_BAR: bool = true;
+
+	/// the height of the space to be reserved for a status bar in pixels
+	pub const BAR_HEIGHT: u32 = 15;
+
+	/// Get [config][penrose::Config]
+	pub fn get() -> Result<Config> {
+		let layouts = crate::layouts();
+		let config = Config::default()
+			.builder()
+			.workspaces(WORKSPACES)
+			.floating_classes(FLOATING_CLASSES)
+			.layouts(layouts)
+			.focused_border(FOCUSED_BORDER_COLOR)?
+			.unfocused_border(UNFOCUSED_BORDER_COLOR)?
+			.border_px(BORDER_WIDTH)
+			.gap_px(GAP_SIZE)
+			.main_ratio_step(MAIN_RATIO_STEP)
+			.show_bar(SHOW_BAR)
+			.top_bar(TOP_BAR)
+			.bar_height(BAR_HEIGHT)
+			.build()?;
+
+		Ok(config)
+	}
+}
+
+/// Get used layout functions
+pub fn layouts() -> Vec<penrose::core::Layout> {
+	vec![]
+}
+
 /// Module containing functions to get key and mouse bindings to be used by
-/// [penrose::WindowManager::grab_keys_and_run] method.
+/// [`grab_keys_and_run`][penrose::WindowManager::grab_keys_and_run] method.
 pub mod bindings {
 	use penrose::{
 		__test_helpers::{KeyBindings, MouseBindings},
@@ -28,7 +92,10 @@ pub mod bindings {
 		Backward, Forward, Less, More,
 	};
 
+	/// Get key bindings
 	pub fn key<X: XConn>() -> KeyBindings<X> {
+		use crate::config::TERMINAL;
+
 		gen_keybindings! {
 			"M-j" => run_internal!(cycle_client, Forward);
 			"M-k" => run_internal!(cycle_client, Backward);
@@ -43,7 +110,7 @@ pub mod bindings {
 			"M-A-Right" => run_internal!(update_main_ratio, More);
 			"M-A-Left" => run_internal!(update_main_ratio, Less);
 			"M-semicolon" => run_external!("dmenu_run");
-			"M-Return" => run_external!("alacritty");
+			"M-Return" => run_external!(TERMINAL);
 			"M-A-Escape" => run_internal!(exit);
 
 			map: { "1", "2", "3", "4", "5", "6", "7", "8", "9" } to index_selectors(9) => {
@@ -53,10 +120,12 @@ pub mod bindings {
 		}
 	}
 
+	/// Get mouse bindings
 	pub fn mouse<X: XConn>() -> MouseBindings<X> {
 		map! {}
 	}
 
+	/// Get both key and mouse bindings
 	pub fn both<X: XConn>() -> (KeyBindings<X>, MouseBindings<X>) {
 		return (key(), mouse());
 	}
@@ -69,7 +138,7 @@ pub mod logger {
 
 	use crate::result::Result;
 
-	/// Initialize simple logger.
+	/// Initialize simple logger
 	pub fn init() -> Result<()> {
 		Ok(TermLogger::init(
 			LevelFilter::Trace,
