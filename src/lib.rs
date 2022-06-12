12f6d7f0c2a036d6bbb7d4f3e149d1295283d612
Mark Meliksetyan <markmelix@gmail.com>
2022-06-12 14:23:39 +0300

    Move logger.rs content to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
 #[macro_use]
 extern crate log;
 
-pub mod logger;
+// Everything related to logging
+pub mod logger {
+    use log::LevelFilter;
+    use simplelog::{ColorChoice, TermLogger};
+
+    use crate::result::Result;
+
+    /// Initialize simple logger.
+    pub fn init() -> Result<()> {
+        Ok(TermLogger::init(
+            LevelFilter::Trace,
+            Default::default(),
+            Default::default(),
+            ColorChoice::Auto,
+        )?)
+    }
+}
 
 pub mod result {
     use std::{error, result};
