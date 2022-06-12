a565696e77f12309e7f8977646eb31bd9c72e203
Mark Meliksetyan <markmelix@gmail.com>
2022-06-12 14:52:10 +0300

    Basic key and mouse bindings functions implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate log;
 
+#[macro_use]
+extern crate penrose;
+
 // Everything related to logging
 pub mod logger {
     use log::LevelFilter;
@@ -23,4 +26,21 @@ pub mod result {
     use std::{error, result};
 
     pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
+}
+
+/// Module containing functions to get key and mouse bindings to be used by
+/// [penrose::WindowManager::grab_keys_and_run] method.
+pub mod bindings {
+    use penrose::new::{
+        bindings::{KeyBindings, MouseBindings},
+        xconnection::XConn,
+    };
+
+    fn key<X: XConn>() -> KeyBindings<X> {
+        map! {}
+    }
+
+    fn mouse<X: XConn>() -> MouseBindings<X> {
+        map! {}
+    }
 }
