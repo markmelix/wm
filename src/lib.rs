0b28c73ec660aa7e8a4e4f819ff7584a49554ac9
Mark Meliksetyan <markmelix@gmail.com>
2022-06-12 15:51:29 +0300

    Now window manager can be run!
    Without bar and good keybindings, but, at least it can be just run.

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,87 @@
 #[macro_use]
-extern crate log;
-
-#[macro_use]
 extern crate penrose;
+
+use result::Result;
+
+pub fn run() -> Result<()> {
+	use penrose::{
+		logging_error_handler, xcb::new_xcb_backed_window_manager, Config,
+	};
+
+	let (key_bindings, mouse_bindings) = bindings::both();
+
+	let mut wm = new_xcb_backed_window_manager(
+		Config::default(),
+		vec![],
+		logging_error_handler(),
+	)?;
+
+	Ok(wm.grab_keys_and_run(key_bindings, mouse_bindings)?)
+}
 
 /// Module containing functions to get key and mouse bindings to be used by
 /// [penrose::WindowManager::grab_keys_and_run] method.
 pub mod bindings {
-    use penrose::{
-        __test_helpers::{KeyBindings, MouseBindings},
-        core::{helpers::index_selectors, xconnection::XConn},
-        Backward, Forward, Less, More,
-    };
+	use penrose::{
+		__test_helpers::{KeyBindings, MouseBindings},
+		core::{helpers::index_selectors, xconnection::XConn},
+		Backward, Forward, Less, More,
+	};
 
-    fn key<X: XConn>() -> KeyBindings<X> {
-        gen_keybindings! {
-            "M-j" => run_internal!(cycle_client, Forward);
-            "M-k" => run_internal!(cycle_client, Backward);
-            "M-S-j" => run_internal!(drag_client, Forward);
-            "M-S-k" => run_internal!(drag_client, Backward);
-            "M-S-q" => run_internal!(kill_client);
-            "M-Tab" => run_internal!(toggle_workspace);
-            "M-grave" => run_internal!(cycle_layout, Forward);
-            "M-S-grave" => run_internal!(cycle_layout, Backward);
-            "M-A-Up" => run_internal!(update_max_main, More);
-            "M-A-Down" => run_internal!(update_max_main, Less);
-            "M-A-Right" => run_internal!(update_main_ratio, More);
-            "M-A-Left" => run_internal!(update_main_ratio, Less);
-            "M-semicolon" => run_external!("dmenu_run");
-            "M-Return" => run_external!("alacritty");
-            "M-A-Escape" => run_internal!(exit);
+	pub fn key<X: XConn>() -> KeyBindings<X> {
+		gen_keybindings! {
+			"M-j" => run_internal!(cycle_client, Forward);
+			"M-k" => run_internal!(cycle_client, Backward);
+			"M-S-j" => run_internal!(drag_client, Forward);
+			"M-S-k" => run_internal!(drag_client, Backward);
+			"M-S-q" => run_internal!(kill_client);
+			"M-Tab" => run_internal!(toggle_workspace);
+			"M-grave" => run_internal!(cycle_layout, Forward);
+			"M-S-grave" => run_internal!(cycle_layout, Backward);
+			"M-A-Up" => run_internal!(update_max_main, More);
+			"M-A-Down" => run_internal!(update_max_main, Less);
+			"M-A-Right" => run_internal!(update_main_ratio, More);
+			"M-A-Left" => run_internal!(update_main_ratio, Less);
+			"M-semicolon" => run_external!("dmenu_run");
+			"M-Return" => run_external!("alacritty");
+			"M-A-Escape" => run_internal!(exit);
 
-            map: { "1", "2", "3", "4", "5", "6", "7", "8", "9" } to index_selectors(9) => {
-                "M-{}" => focus_workspace (REF);
-                "M-S-{}" => client_to_workspace (REF);
-            };
-        }
-    }
+			map: { "1", "2", "3", "4", "5", "6", "7", "8", "9" } to index_selectors(9) => {
+				"M-{}" => focus_workspace (REF);
+				"M-S-{}" => client_to_workspace (REF);
+			};
+		}
+	}
 
-    fn mouse<X: XConn>() -> MouseBindings<X> {
-        map! {}
-    }
+	pub fn mouse<X: XConn>() -> MouseBindings<X> {
+		map! {}
+	}
+
+	pub fn both<X: XConn>() -> (KeyBindings<X>, MouseBindings<X>) {
+		return (key(), mouse());
+	}
 }
 
 // Everything related to logging
 pub mod logger {
-    use log::LevelFilter;
-    use simplelog::{ColorChoice, TermLogger};
+	use log::LevelFilter;
+	use simplelog::{ColorChoice, TermLogger};
 
-    use crate::result::Result;
+	use crate::result::Result;
 
-    /// Initialize simple logger.
-    pub fn init() -> Result<()> {
-        Ok(TermLogger::init(
-            LevelFilter::Trace,
-            Default::default(),
-            Default::default(),
-            ColorChoice::Auto,
-        )?)
-    }
+	/// Initialize simple logger.
+	pub fn init() -> Result<()> {
+		Ok(TermLogger::init(
+			LevelFilter::Trace,
+			Default::default(),
+			Default::default(),
+			ColorChoice::Auto,
+		)?)
+	}
 }
 
 pub mod result {
-    use std::{error, result};
+	use std::{error, result};
 
-    pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
+	pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,20 @@ use std::process;
 use wm::logger;
 
 fn main() {
-    match logger::init() {
-        Ok(_) => info!("Logger has been initialized successfully"),
-        Err(e) => {
-            eprintln!("Failed to initialize logger: {e}");
-            process::exit(1);
-        }
-    }
+	match logger::init() {
+		Ok(_) => info!("Logger has been initialized successfully"),
+		Err(e) => {
+			eprintln!("Failed to initialize logger: {e}");
+			process::exit(1);
+		}
+	}
+
+	info!("Running window manager");
+
+	if let Err(e) = wm::run() {
+		error!("{e}");
+		process::exit(1);
+	}
+
+	info!("Exited from the window manager successfully");
 }
