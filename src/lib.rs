e6c1f86f34f5a445cd4d813d3d393c9b20b3d494
Mark Meliksetyan <markmelix@gmail.com>
2022-06-12 15:20:00 +0300

    Relocate modules in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,6 @@ extern crate log;
 #[macro_use]
 extern crate penrose;
 
-// Everything related to logging
-pub mod logger {
-    use log::LevelFilter;
-    use simplelog::{ColorChoice, TermLogger};
-
-    use crate::result::Result;
-
-    /// Initialize simple logger.
-    pub fn init() -> Result<()> {
-        Ok(TermLogger::init(
-            LevelFilter::Trace,
-            Default::default(),
-            Default::default(),
-            ColorChoice::Auto,
-        )?)
-    }
-}
-
-pub mod result {
-    use std::{error, result};
-
-    pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
-}
-
 /// Module containing functions to get key and mouse bindings to be used by
 /// [penrose::WindowManager::grab_keys_and_run] method.
 pub mod bindings {
@@ -65,4 +41,28 @@ pub mod bindings {
     fn mouse<X: XConn>() -> MouseBindings<X> {
         map! {}
     }
+}
+
+// Everything related to logging
+pub mod logger {
+    use log::LevelFilter;
+    use simplelog::{ColorChoice, TermLogger};
+
+    use crate::result::Result;
+
+    /// Initialize simple logger.
+    pub fn init() -> Result<()> {
+        Ok(TermLogger::init(
+            LevelFilter::Trace,
+            Default::default(),
+            Default::default(),
+            ColorChoice::Auto,
+        )?)
+    }
+}
+
+pub mod result {
+    use std::{error, result};
+
+    pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
 }
